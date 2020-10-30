d7dafe578fab7bc2d37c9e3f36dfa5e037d6f9e8
tpmccrary <timothypmccrary@gmail.com>
2020-10-30 10:32:45 -0600

    Commented out test.dart

--- test/c4_app_test.dart
+++ test/c4_app_test.dart
@@ -1,8 +1,8 @@
-import 'package:c4_app/c4_app.dart';
-import 'package:test/test.dart';
+// import 'package:c4_app/c4_app.dart';
+// import 'package:test/test.dart';
 
-void main() {
-  // test('calculate', () {
-  //   expect(calculate(), 42);
-  // });
-}
+// void main() {
+//   // test('calculate', () {
+//   //   expect(calculate(), 42);
+//   // });
+// }
